974e2164defb3095884dcd6978fdc28bdbfe7b38
Andrew Keenan Richardson <andrew@generallyintelligent.com>
2024-06-04 14:36:44 -0700

    aider: Add a new line between "Hello" and the ">" character.
    # Aider chat conversation:
    
    USER: Put a new line between the Hello and the >
    
    ASSISTANT: Here is the change to add a new line between "Hello" and the ">":
    
    aider/io.py
    ```python
    <<<<<<< SEARCH
            show += "Hello> "
    =======
            show += "Hello\n> "
    >>>>>>> REPLACE
    ```

--- aider/io.py
+++ aider/io.py
@@ -191,7 +191,7 @@ class InputOutput:
         show = " ".join(rel_fnames)
         if len(show) > 10:
             show += "\n"
-        show += "Hello> "
+        show += "Hello\n> "
 
         inp = ""
         multiline_input = False
